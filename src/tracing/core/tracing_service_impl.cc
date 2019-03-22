1a94623851b10a7b52ab1008332a6ca56df3d5c3
Stephen Nusko <nuskos@google.com>
2019-03-22 13:19:10 +0000

    Add a new TrustedPacket for received triggers.
    We create a message that contains the time a trigger was received and
    a copy of the trigger itself. This allows us to emit whatever triggers
    we've received into the trace and the UI will be able to annotate the
    trace based on these packets. However for STOP_TRACE this is only
    correct as of the first ReadBuffers call, more triggers could be
    received later and won't be reemitted. This is due to difficultly in
    knowing when to put them in the correct time order.
    
    Bug: 128966650
    Change-Id: Ib751f4f2547cc2c8f68161af7437de29395d342b

--- src/tracing/core/tracing_service_impl.cc
+++ src/tracing/core/tracing_service_impl.cc
@@ -901,14 +901,11 @@ void TracingServiceImpl::ActivateTriggers(
         continue;
       }
 
-      // TODO(nuskos): Currently we store these triggers but don't actually
-      // return them inside ReadBuffers. In a followup CL add this
-      // functionality. Ensure that there is no race condition between
-      // future triggers being added and ReadBuffers processing this vector.
       const bool triggers_already_received =
           !tracing_session.received_triggers.empty();
-      tracing_session.received_triggers.push_back(std::make_pair(
-          static_cast<uint64_t>(base::GetBootTimeNs().count()), *iter));
+      tracing_session.received_triggers.push_back(
+          {static_cast<uint64_t>(base::GetBootTimeNs().count()), iter->name(),
+           producer->name_, producer->uid_});
       auto weak_this = weak_ptr_factory_.GetWeakPtr();
       switch (tracing_session.config.trigger_config().trigger_mode()) {
         case TraceConfig::TriggerConfig::START_TRACING:
@@ -1353,6 +1350,7 @@ void TracingServiceImpl::ReadBuffers(TracingSessionID tsid,
   }
   MaybeEmitTraceConfig(tracing_session, &packets);
   MaybeEmitSystemInfo(tracing_session, &packets);
+  MaybeEmitReceivedTriggers(tracing_session, &packets);
 
   size_t packets_bytes = 0;  // SUM(slice.size() for each slice in |packets|).
   size_t total_slices = 0;   // SUM(#slices in |packets|).
@@ -2130,6 +2128,34 @@ void TracingServiceImpl::MaybeEmitSystemInfo(
     utsname_info->set_release(uname_info.release);
   }
 #endif
+  packet.set_trusted_uid(static_cast<int32_t>(uid_));
+  packet.set_trusted_packet_sequence_id(kServicePacketSequenceID);
+  Slice slice = Slice::Allocate(static_cast<size_t>(packet.ByteSize()));
+  PERFETTO_CHECK(packet.SerializeWithCachedSizesToArray(slice.own_data()));
+  packets->emplace_back();
+  packets->back().AddSlice(std::move(slice));
+}
+
+void TracingServiceImpl::MaybeEmitReceivedTriggers(
+    TracingSession* tracing_session,
+    std::vector<TracePacket>* packets) {
+  if (tracing_session->num_emitted_received_triggers ==
+      tracing_session->received_triggers.size()) {
+    return;
+  }
+  protos::TrustedPacket packet;
+  protos::Triggers* triggers = packet.mutable_triggers();
+  for (size_t i = tracing_session->num_emitted_received_triggers;
+       i < tracing_session->received_triggers.size(); ++i) {
+    const auto& info = tracing_session->received_triggers[i];
+    auto* trigger = triggers->add_triggers();
+    trigger->set_boot_time_ns(info.boot_time_ns);
+    trigger->set_trigger_name(info.trigger_name);
+    trigger->set_producer_name(info.producer_name);
+    trigger->set_producer_uid(static_cast<int32_t>(info.producer_uid));
+  }
+  tracing_session->num_emitted_received_triggers =
+      tracing_session->received_triggers.size();
   packet.set_trusted_uid(static_cast<int32_t>(uid_));
   packet.set_trusted_packet_sequence_id(kServicePacketSequenceID);
   Slice slice = Slice::Allocate(static_cast<size_t>(packet.ByteSize()));
