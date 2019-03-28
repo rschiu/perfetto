b83d6e5b7580e5c02a8d0378c20ecb14d904c297
Florian Mayer <fmayer@google.com>
2019-03-28 11:09:46 +0000

    [Patchset 1] Allow to specify heapprofd shmem size in config.
    Change-Id: I06b842bd0dd123ca50fbd14147396d5c29c92925

--- src/profiling/memory/heapprofd_producer.cc
+++ src/profiling/memory/heapprofd_producer.cc
@@ -42,8 +42,7 @@ constexpr int kHeapprofdSignal = 36;
 constexpr uint32_t kInitialConnectionBackoffMs = 100;
 constexpr uint32_t kMaxConnectionBackoffMs = 30 * 1000;
 
-// TODO(fmayer): Add to HeapprofdConfig.
-constexpr uint64_t kShmemSize = 8 * 1048576;  // ~8 MB
+constexpr uint64_t kDefaultShmemSize = 8 * 1048576;  // ~8 MB
 
 ClientConfiguration MakeClientConfiguration(const DataSourceConfig& cfg) {
   ClientConfiguration client_config;
@@ -629,7 +628,11 @@ void HeapprofdProducer::HandleClientConnection(
   }
   RecordOtherSourcesAsRejected(data_source, process);
 
-  auto shmem = SharedRingBuffer::Create(kShmemSize);
+  uint64_t shmem_size = data_source->config.shmem_size();
+  if (!shmem_size)
+    shmem_size = kDefaultShmemSize;
+
+  auto shmem = SharedRingBuffer::Create();
   if (!shmem || !shmem->is_valid()) {
     PERFETTO_LOG("Failed to create shared memory.");
     return;
