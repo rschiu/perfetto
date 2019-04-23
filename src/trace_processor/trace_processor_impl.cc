0bd149e69a87cb98798827d3bfdbbd56510fe86d
Lalit Maganti <lalitm@google.com>
2019-04-23 18:26:18 +0100

    trace_processor: implement first android mem metric!
    We fill the LMK field for now: more metrics will be filled in following
    CLs.
    
    Context: go/perfetto-metrics
    Bug: 129747127
    Change-Id: If3d7de375e16bbfd15a8b19bda05d3162ae30308

--- src/trace_processor/trace_processor_impl.cc
+++ src/trace_processor/trace_processor_impl.cc
@@ -22,6 +22,7 @@
 
 #include "perfetto/base/logging.h"
 #include "perfetto/base/time.h"
+#include "perfetto/protozero/scattered_heap_buffer.h"
 #include "src/trace_processor/android_logs_table.h"
 #include "src/trace_processor/args_table.h"
 #include "src/trace_processor/args_tracker.h"
@@ -30,6 +31,7 @@
 #include "src/trace_processor/counter_values_table.h"
 #include "src/trace_processor/event_tracker.h"
 #include "src/trace_processor/instants_table.h"
+#include "src/trace_processor/metrics/sql_metrics.h"
 #include "src/trace_processor/process_table.h"
 #include "src/trace_processor/process_tracker.h"
 #include "src/trace_processor/proto_trace_parser.h"
@@ -49,6 +51,9 @@
 #include "src/trace_processor/trace_blob_view.h"
 #include "src/trace_processor/trace_sorter.h"
 #include "src/trace_processor/window_operator_table.h"
+
+#include "perfetto/metrics/android/mem_metric.pbzero.h"
+#include "perfetto/metrics/metrics.pbzero.h"
 
 // JSON parsing is only supported in the standalone build.
 #if PERFETTO_BUILDFLAG(PERFETTO_STANDALONE_BUILD)
@@ -149,6 +154,29 @@ std::string RemoveWhitespace(const std::string& input) {
   std::string str(input);
   str.erase(std::remove_if(str.begin(), str.end(), ::isspace), str.end());
   return str;
+}
+
+std::vector<std::string> SplitString(const std::string& text,
+                                     const std::string& delimiter) {
+  std::vector<std::string> output;
+  size_t start = 0;
+  for (size_t i = 0; i < text.size(); i++) {
+    bool matches = true;
+    size_t j = i;
+    for (char c : delimiter) {
+      if (text[j++] != c) {
+        matches = false;
+        break;
+      }
+    }
+    if (matches) {
+      output.emplace_back(&text[start], i - start);
+      start = j;
+    }
+  }
+  if (start < text.size())
+    output.emplace_back(&text[start], text.size() - start);
+  return output;
 }
 
 }  // namespace
@@ -278,7 +306,48 @@ void TraceProcessorImpl::InterruptQuery() {
 int TraceProcessorImpl::ComputeMetric(
     const std::vector<std::string>& metric_names,
     std::vector<uint8_t>* metrics_proto) {
-  perfetto::base::ignore_result(metric_names, metrics_proto);
+  if (metric_names.size() != 1 || metric_names[0] != "android.mem") {
+    PERFETTO_ELOG("Only android.mem metric is currently supported");
+    return 1;
+  }
+
+  auto queries = SplitString(metrics::kAndroidMem, ";\n\n");
+  for (const auto& query : queries) {
+    PERFETTO_DLOG("Executing query: %s", query.c_str());
+    auto prep_it = ExecuteQuery(query);
+    auto prep_has_next = prep_it.Next();
+    if (auto opt_error = prep_it.GetLastError()) {
+      PERFETTO_ELOG("SQLite error: %s", opt_error->c_str());
+      return 1;
+    }
+    PERFETTO_DCHECK(!prep_has_next);
+  }
+
+  protozero::ScatteredHeapBuffer delegate;
+  protozero::ScatteredStreamWriter writer(&delegate);
+  delegate.set_writer(&writer);
+
+  protos::pbzero::TraceMetrics metrics;
+  metrics.Reset(&writer);
+
+  auto it = ExecuteQuery("SELECT COUNT(*) from lmk_by_score;");
+  auto has_next = it.Next();
+  if (auto opt_error = it.GetLastError()) {
+    PERFETTO_ELOG("SQLite error: %s", opt_error->c_str());
+    return 1;
+  }
+  PERFETTO_CHECK(has_next);
+  PERFETTO_CHECK(it.Get(0).type == SqlValue::Type::kLong);
+
+  auto* memory = metrics.set_android_mem();
+  memory->set_system_metrics()->set_lmks()->set_total_count(
+      static_cast<int32_t>(it.Get(0).long_value));
+
+  *metrics_proto = delegate.StitchSlices();
+
+  has_next = it.Next();
+  PERFETTO_DCHECK(!has_next);
+
   return 0;
 }
 
