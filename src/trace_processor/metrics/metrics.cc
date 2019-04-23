69f49df773551fcdfdc7907ecee7d58f5816c349
Lalit Maganti <lalitm@google.com>
2019-04-23 18:29:58 +0100

    trace_processor: add support for creating protos from SQL
    Implements go/perfetto-metrics-sql-proto for trace processor.
    
    Bug: 129747127
    Change-Id: Id8516f7478850009baee117a988b9d9598a15e49

--- src/trace_processor/metrics/metrics.cc
+++ src/trace_processor/metrics/metrics.cc
@@ -19,10 +19,13 @@
 #include <unordered_map>
 #include <vector>
 
+#include "perfetto/protozero/scattered_heap_buffer.h"
+#include "src/trace_processor/metrics/metrics.descriptor.h"
+#include "src/trace_processor/metrics/sql_metrics.h"
+
+#include "perfetto/common/descriptor.pbzero.h"
 #include "perfetto/metrics/android/mem_metric.pbzero.h"
 #include "perfetto/metrics/metrics.pbzero.h"
-#include "perfetto/protozero/scattered_heap_buffer.h"
-#include "src/trace_processor/metrics/sql_metrics.h"
 
 namespace perfetto {
 namespace trace_processor {
@@ -91,12 +94,160 @@ int TemplateReplace(
   return sub_start_idx == -1 ? 0 : 3;
 }
 
-struct FunctionContext {
+class FieldDescriptor {
+ public:
+  FieldDescriptor(std::string name,
+                  uint32_t number,
+                  uint32_t type,
+                  std::string raw_type_name)
+      : name_(std::move(name)),
+        number_(number),
+        type_(type),
+        raw_type_name_(std::move(raw_type_name)) {}
+
+  void SetMessageTypeIdx(uint32_t idx) { message_type_idx_ = idx; }
+
+  const std::string& name() const { return name_; }
+  uint32_t number() const { return number_; }
+  uint32_t type() const { return type_; }
+  const std::string& raw_type_name() const { return raw_type_name_; }
+
+ private:
+  std::string name_;
+  uint32_t number_;
+  uint32_t type_;
+  std::string raw_type_name_;
+  base::Optional<uint32_t> message_type_idx_;
+};
+
+class ProtoDescriptor {
+ public:
+  ProtoDescriptor(std::string package_name,
+                  std::string full_name,
+                  base::Optional<uint32_t> parent_id)
+      : package_name_(std::move(package_name)),
+        full_name_(std::move(full_name)),
+        parent_id_(parent_id) {}
+
+  void AddField(FieldDescriptor descriptor) {
+    fields_.emplace_back(std::move(descriptor));
+  }
+
+  base::Optional<uint32_t> FindFieldIdx(const std::string& name) const {
+    auto it = std::find_if(
+        fields_.begin(), fields_.end(),
+        [name](const FieldDescriptor& desc) { return desc.name() == name; });
+    auto idx = static_cast<uint32_t>(std::distance(fields_.begin(), it));
+    return idx < fields_.size() ? base::Optional<uint32_t>(idx) : base::nullopt;
+  }
+
+  const std::string& package_name() const { return package_name_; }
+
+  const std::string& full_name() const { return full_name_; }
+
+  const std::vector<FieldDescriptor>& fields() const { return fields_; }
+  std::vector<FieldDescriptor>* mutable_fields() { return &fields_; }
+
+ private:
+  std::string package_name_;
+  std::string full_name_;
+  base::Optional<uint32_t> parent_id_;
+  std::vector<FieldDescriptor> fields_;
+};
+
+class DescriptorPool {
+ public:
+  uint32_t AddDescriptor(ProtoDescriptor descriptor) {
+    descriptors_.emplace_back(std::move(descriptor));
+    return static_cast<uint32_t>(descriptors_.size()) - 1;
+  }
+
+  base::Optional<uint32_t> FindDescriptorIdx(
+      const std::string& full_name) const {
+    auto it = std::find_if(descriptors_.begin(), descriptors_.end(),
+                           [full_name](const ProtoDescriptor& desc) {
+                             return desc.full_name() == full_name;
+                           });
+    auto idx = static_cast<uint32_t>(std::distance(descriptors_.begin(), it));
+    return idx < descriptors_.size() ? base::Optional<uint32_t>(idx)
+                                     : base::nullopt;
+  }
+
+  const std::vector<ProtoDescriptor>& descriptors() const {
+    return descriptors_;
+  }
+  std::vector<ProtoDescriptor>* mutable_descriptors() { return &descriptors_; }
+
+ private:
+  std::vector<ProtoDescriptor> descriptors_;
+};
+
+void AddAllProtoDescriptors(const std::string& package_name,
+                            base::Optional<uint32_t> parent_idx,
+                            protos::pbzero::DescriptorProto::Decoder decoder,
+                            DescriptorPool* pool) {
+  auto parent_name =
+      parent_idx ? pool->descriptors()[*parent_idx].full_name() : package_name;
+  auto full_name =
+      parent_name + "." + base::StringView(decoder.name()).ToStdString();
+
+  using FieldDescriptorProto = protos::pbzero::FieldDescriptorProto;
+  ProtoDescriptor proto_descriptor(package_name, full_name, parent_idx);
+  for (auto it = decoder.field(); it; ++it) {
+    FieldDescriptorProto::Decoder f_decoder(it->data(), it->size());
+    FieldDescriptor field(
+        base::StringView(f_decoder.name()).ToStdString(),
+        static_cast<uint32_t>(f_decoder.number()),
+        static_cast<uint32_t>(f_decoder.type()),
+        base::StringView(f_decoder.type_name()).ToStdString());
+    proto_descriptor.AddField(std::move(field));
+  }
+
+  uint32_t idx = pool->AddDescriptor(std::move(proto_descriptor));
+  for (auto it = decoder.nested_type(); it; ++it) {
+    protos::pbzero::DescriptorProto::Decoder nested(it->data(), it->size());
+    AddAllProtoDescriptors(package_name, idx, std::move(nested), pool);
+  }
+}
+
+DescriptorPool GetMetricsProtoDescriptorPool() {
+  DescriptorPool pool;
+
+  // First pass: extract all the message descriptors from the file and add them
+  // to the pool.
+  protos::pbzero::FileDescriptorSet::Decoder proto(kMetricsDescriptor.data(),
+                                                   kMetricsDescriptor.size());
+  for (auto it = proto.file(); it; ++it) {
+    protos::pbzero::FileDescriptorProto::Decoder file(it->data(), it->size());
+    std::string package = "." + base::StringView(file.package()).ToStdString();
+
+    using DescriptorProto = protos::pbzero::DescriptorProto;
+    for (auto message_it = file.message_type(); message_it; ++message_it) {
+      DescriptorProto::Decoder decoder(message_it->data(), message_it->size());
+      AddAllProtoDescriptors(package, base::nullopt, std::move(decoder), &pool);
+    }
+  }
+
+  // Second pass: resolve the types of all the fields to the correct indiices.
+  using FieldDescriptorProto = protos::pbzero::FieldDescriptorProto;
+  for (auto& descriptor : *pool.mutable_descriptors()) {
+    for (auto& field : *descriptor.mutable_fields()) {
+      if (field.type() == FieldDescriptorProto::TYPE_MESSAGE ||
+          field.type() == FieldDescriptorProto::TYPE_ENUM) {
+        field.SetMessageTypeIdx(
+            pool.FindDescriptorIdx(field.raw_type_name()).value());
+      }
+    }
+  }
+  return pool;
+}
+
+struct RunMetricFunctionContext {
   TraceProcessorImpl* tp;
 };
 
 void RunMetric(sqlite3_context* ctx, int argc, sqlite3_value** argv) {
-  auto* fn_ctx = static_cast<FunctionContext*>(sqlite3_user_data(ctx));
+  auto* fn_ctx = static_cast<RunMetricFunctionContext*>(sqlite3_user_data(ctx));
   if (argc % 2 != 1 || sqlite3_value_type(argv[0]) != SQLITE_TEXT) {
     sqlite3_result_error(ctx, "Invalid call to RUN_METRIC", -1);
     return;
@@ -132,7 +283,7 @@ void RunMetric(sqlite3_context* ctx, int argc, sqlite3_value** argv) {
       return;
     }
 
-    PERFETTO_LOG("RUN_METRIC: Executing query: %s", buffer.data());
+    PERFETTO_DLOG("RUN_METRIC: Executing query: %s", buffer.data());
     auto it = fn_ctx->tp->ExecuteQuery(buffer.data());
     if (auto opt_error = it.GetLastError()) {
       sqlite3_result_error(ctx, "RUN_METRIC: Error when running file", -1);
@@ -142,6 +293,122 @@ void RunMetric(sqlite3_context* ctx, int argc, sqlite3_value** argv) {
       return;
     }
   }
+}
+
+struct BuildProtoFunctionContext {
+  TraceProcessorImpl* tp;
+  DescriptorPool* pool;
+  std::string proto_name;
+};
+
+void BuildProto(sqlite3_context* ctx, int argc, sqlite3_value** argv) {
+  auto* fn_ctx =
+      static_cast<BuildProtoFunctionContext*>(sqlite3_user_data(ctx));
+  if (argc % 2 != 0) {
+    sqlite3_result_error(ctx, "Invalid call to BuildProto", -1);
+    return;
+  }
+
+  auto opt_idx = fn_ctx->pool->FindDescriptorIdx(fn_ctx->proto_name);
+  const auto& desc = fn_ctx->pool->descriptors()[opt_idx.value()];
+
+  protozero::ScatteredHeapBuffer delegate;
+  protozero::ScatteredStreamWriter writer(&delegate);
+  delegate.set_writer(&writer);
+
+  protozero::Message message;
+  message.Reset(&writer);
+
+  for (int i = 0; i < argc; i += 2) {
+    if (sqlite3_value_type(argv[i]) != SQLITE_TEXT) {
+      sqlite3_result_error(ctx, "BuildProto: Invalid args", -1);
+      return;
+    }
+
+    auto* key_str = reinterpret_cast<const char*>(sqlite3_value_text(argv[i]));
+    auto opt_field_idx = desc.FindFieldIdx(key_str);
+    const auto& field = desc.fields()[opt_field_idx.value()];
+
+    using FieldDescriptorProto = protos::pbzero::FieldDescriptorProto;
+    auto* value = argv[i + 1];
+    switch (field.type()) {
+      case FieldDescriptorProto::TYPE_INT32:
+      case FieldDescriptorProto::TYPE_INT64:
+      case FieldDescriptorProto::TYPE_UINT32:
+      case FieldDescriptorProto::TYPE_BOOL:
+        if (sqlite3_value_type(value) != SQLITE_INTEGER) {
+          sqlite3_result_error(ctx, "BuildProto: field has wrong type", -1);
+          return;
+        }
+        message.AppendVarInt(field.number(), sqlite3_value_int64(value));
+        break;
+      case FieldDescriptorProto::TYPE_SINT32:
+      case FieldDescriptorProto::TYPE_SINT64:
+        if (sqlite3_value_type(value) != SQLITE_INTEGER) {
+          sqlite3_result_error(ctx, "BuildProto: field has wrong type", -1);
+          return;
+        }
+        message.AppendSignedVarInt(field.number(), sqlite3_value_int64(value));
+        break;
+      case FieldDescriptorProto::TYPE_FIXED32:
+      case FieldDescriptorProto::TYPE_SFIXED32:
+      case FieldDescriptorProto::TYPE_FIXED64:
+      case FieldDescriptorProto::TYPE_SFIXED64:
+        if (sqlite3_value_type(value) != SQLITE_INTEGER) {
+          sqlite3_result_error(ctx, "BuildProto: field has wrong type", -1);
+          return;
+        }
+        message.AppendFixed(field.number(), sqlite3_value_int64(value));
+        break;
+      case FieldDescriptorProto::TYPE_FLOAT:
+      case FieldDescriptorProto::TYPE_DOUBLE:
+        if (sqlite3_value_type(value) != SQLITE_FLOAT) {
+          sqlite3_result_error(ctx, "BuildProto: field has wrong type", -1);
+          return;
+        }
+        message.AppendFixed(field.number(), sqlite3_value_double(value));
+        break;
+      case FieldDescriptorProto::TYPE_STRING: {
+        if (sqlite3_value_type(value) != SQLITE_TEXT) {
+          sqlite3_result_error(ctx, "BuildProto: field has wrong type", -1);
+          return;
+        }
+        auto* text = reinterpret_cast<const char*>(sqlite3_value_text(value));
+        message.AppendString(field.number(), text);
+        break;
+      }
+      case FieldDescriptorProto::TYPE_MESSAGE: {
+        // TODO(lalitm): verify the type of the nested message.
+        if (sqlite3_value_type(value) != SQLITE_BLOB) {
+          sqlite3_result_error(ctx, "BuildProto: field has wrong type", -1);
+          return;
+        }
+        auto* blob = sqlite3_value_blob(value);
+        auto bytes = static_cast<size_t>(sqlite3_value_bytes(value));
+        message.AppendBytes(field.number(), blob, bytes);
+        break;
+      }
+      case FieldDescriptorProto::TYPE_UINT64:
+        sqlite3_result_error(ctx, "BuildProto: uint64_t unsupported", -1);
+        return;
+      case FieldDescriptorProto::TYPE_GROUP:
+        sqlite3_result_error(ctx, "BuildProto: groups unsupported", -1);
+        return;
+      case FieldDescriptorProto::TYPE_ENUM:
+        // TODO(lalit): add support for enums.
+        sqlite3_result_error(ctx, "BuildProto: enums unsupported", -1);
+        return;
+    }
+  }
+  message.Finalize();
+
+  // TODO(lalitm): this is horribly inefficient but since our protos are small,
+  // it will do for now. Change this to not have so many copies.
+  auto slices = delegate.StitchSlices();
+  std::unique_ptr<uint8_t[]> data(static_cast<uint8_t*>(malloc(slices.size())));
+  memcpy(data.get(), slices.data(), slices.size());
+  sqlite3_result_blob(ctx, data.release(), static_cast<int>(slices.size()),
+                      free);
 }
 
 }  // namespace
@@ -154,13 +421,34 @@ int ComputeMetrics(TraceProcessorImpl* tp,
     return 1;
   }
 
-  std::unique_ptr<FunctionContext> ctx(new FunctionContext());
+  std::unique_ptr<RunMetricFunctionContext> ctx(new RunMetricFunctionContext());
   ctx->tp = tp;
   auto ret =
       tp->RegisterScalarFunction("RUN_METRIC", -1, std::move(ctx), &RunMetric);
   if (ret) {
     PERFETTO_ELOG("SQLite error: %d", ret);
     return ret;
+  }
+
+  DescriptorPool pool = GetMetricsProtoDescriptorPool();
+  for (const auto& desc : pool.descriptors()) {
+    std::unique_ptr<BuildProtoFunctionContext> proto_fn_ctx(
+        new BuildProtoFunctionContext());
+    proto_fn_ctx->tp = tp;
+    proto_fn_ctx->pool = &pool;
+    proto_fn_ctx->proto_name = desc.full_name();
+
+    // Convert the full name (e.g. .perfetto.protos.TraceMetrics.SubMetric)
+    // into a function name of the form (TraceMetrics_SubMetric).
+    auto fn_name = desc.full_name().substr(desc.package_name().size() + 1);
+    std::replace(fn_name.begin(), fn_name.end(), '.', '_');
+
+    ret = tp->RegisterScalarFunction(fn_name, -1, std::move(proto_fn_ctx),
+                                     &BuildProto);
+    if (ret) {
+      PERFETTO_ELOG("SQLite error: %d", ret);
+      return ret;
+    }
   }
 
   for (const auto& query : SplitString(sql_metrics::kAndroidMem, ";\n")) {
@@ -174,48 +462,31 @@ int ComputeMetrics(TraceProcessorImpl* tp,
     PERFETTO_DCHECK(!prep_has_next);
   }
 
-  protozero::ScatteredHeapBuffer delegate;
-  protozero::ScatteredStreamWriter writer(&delegate);
-  delegate.set_writer(&writer);
-
-  protos::pbzero::TraceMetrics metrics;
-  metrics.Reset(&writer);
-
-  auto it = tp->ExecuteQuery("SELECT COUNT(*) from lmk_by_score;");
+  auto it = tp->ExecuteQuery("SELECT * from Output;");
   auto has_next = it.Next();
   if (auto opt_error = it.GetLastError()) {
     PERFETTO_ELOG("SQLite error: %s", opt_error->c_str());
     return 1;
-  }
-  PERFETTO_CHECK(has_next);
-  PERFETTO_CHECK(it.Get(0).type == SqlValue::Type::kLong);
-
-  has_next = it.Next();
-  PERFETTO_DCHECK(!has_next);
-
-  auto* memory = metrics.set_android_mem();
-  memory->set_system_metrics()->set_lmks()->set_total_count(
-      static_cast<int32_t>(it.Get(0).long_value));
-
-  it = tp->ExecuteQuery("SELECT * from anon_rss;");
-  while (it.Next()) {
-    const char* name = it.Get(0).string_value;
-
-    auto* process = memory->add_process_metrics();
-    process->set_process_name(name);
-
-    auto* anon = process->set_overall_counters()->set_anon_rss();
-    anon->set_min(it.Get(1).AsDouble());
-    anon->set_max(it.Get(2).AsDouble());
-    anon->set_avg(it.Get(3).AsDouble());
-  }
-  if (auto opt_error = it.GetLastError()) {
-    PERFETTO_ELOG("SQLite error: %s", opt_error->c_str());
+  } else if (!has_next) {
+    PERFETTO_ELOG("Output table should have at least one row");
+    return 1;
+  } else if (it.ColumnCount() != 1) {
+    PERFETTO_ELOG("Output table should have exactly one column");
+    return 1;
+  } else if (it.Get(0).type != SqlValue::kBytes) {
+    PERFETTO_ELOG("Output table column should have type bytes");
     return 1;
   }
 
-  metrics.Finalize();
-  *metrics_proto = delegate.StitchSlices();
+  const uint8_t* ptr = static_cast<const uint8_t*>(it.Get(0).bytes_value);
+  *metrics_proto = std::vector<uint8_t>(ptr, ptr + it.Get(0).bytes_count);
+
+  has_next = it.Next();
+  if (has_next) {
+    PERFETTO_ELOG("Output table should only have one row");
+    return 1;
+  }
+
   return 0;
 }
 
